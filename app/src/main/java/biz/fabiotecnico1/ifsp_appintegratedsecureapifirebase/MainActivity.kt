5e98918dae352fd3d154d831e37c3b09c7466b9c
Fabio Luis <ml@fabiotecnico1.biz>
2026-06-16 21:43:47 -0300

    chore: adequação do projeto para Android Studio 4.2.2 e API 16 - Configurado o Kotlin, ViewBinding e dependências base - Definido o minSdk=16 (Jelly Bean) - Adicionado suporte para Coroutines e Retrofit - Preparado a estrutura de pacotes

---
+++ app/src/main/java/biz/fabiotecnico1/ifsp_appintegratedsecureapifirebase/MainActivity.kt
@@ -0,0 +1,11 @@
+package biz.fabiotecnico1.ifsp_appintegratedsecureapifirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
